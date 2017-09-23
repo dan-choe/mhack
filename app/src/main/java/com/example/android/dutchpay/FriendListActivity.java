03e31f14efde3bbfba44b781d62117e135d0bd86
dongjun seung <dongjun2@illinois.edu>
2017-09-23 19:29:47 -0400

    user

--- app/src/main/java/com/example/android/dutchpay/FriendListActivity.java
+++ app/src/main/java/com/example/android/dutchpay/FriendListActivity.java
@@ -1,8 +1,6 @@
 package com.example.android.dutchpay;
 
-/**
- * Created by brians2016 on 9/23/17.
- */
 
 public class FriendListActivity {
+    
 }
--- app/src/main/java/com/example/android/dutchpay/User.java
+++ app/src/main/java/com/example/android/dutchpay/User.java
@@ -27,4 +27,51 @@ public class User {
         this.friendList = friendList;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public List<String> getFriendList() {
+        return friendList;
+    }
+
+    public void setFriendList(List<String> friendList) {
+        this.friendList = friendList;
+    }
 }
