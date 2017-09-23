6b37f70dfe96dc5a08e43ecf7ce6b44a26319257
hackchoi <seunghyuk.choi@stonybrook.edu>
2017-09-23 16:12:48 -0400

    -Allowing user to override total amount if it's wrong. -Clicking discard button makes it going back to selection screen.

--- app/src/main/java/com/example/android/dutchpay/ConfirmActivity.java
+++ app/src/main/java/com/example/android/dutchpay/ConfirmActivity.java
@@ -1,16 +1,21 @@
 package com.example.android.dutchpay;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.FileNotFoundException;
 
 public class ConfirmActivity extends AppCompatActivity implements View.OnClickListener {
     private ImageView receipt_image;
+
+    private Button ok_btn;
+    private Button discard_btn;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,9 +26,20 @@ public class ConfirmActivity extends AppCompatActivity implements View.OnClickLi
             receipt_image.setImageBitmap(bitmap);
         }catch (FileNotFoundException e) {
         }
+        ok_btn = (Button)findViewById(R.id.ok_btn);
+        ok_btn.setOnClickListener(this);
+
+        discard_btn = (Button)findViewById(R.id.discard_btn);
+        discard_btn.setOnClickListener(this);
     }
 
+    @Override
     public void onClick(View v) {
+        if (v == ok_btn) {
 
+        }
+        if (v == discard_btn ) {
+            finish();
+        }
     }
 }
