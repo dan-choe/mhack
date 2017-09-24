61df9d2ac24eeca7f06a900f69ea01a6c631a172
dongjun seung <dongjun2@illinois.edu>
2017-09-23 22:55:42 -0400

    recycle

--- app/src/main/java/com/example/android/dutchpay/FriendListActivity.java
+++ app/src/main/java/com/example/android/dutchpay/FriendListActivity.java
@@ -1,115 +1,68 @@
 package com.example.android.dutchpay;
 
-
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.widget.CompoundButton;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Switch;
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class FriendListActivity extends AppCompatActivity{
+
+public class FriendListActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
+    private List<User> nFriends = new ArrayList<>();
+    private FriendRecycleAdapter nFriendAdapter;
     private DatabaseReference mDataReference;
-    private List<String> checkedFriends;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
 
     @Override
-    protected void onCreate(Bundle savedInstance) {
-        super.onCreate(savedInstance);
-        setContentView(R.layout.friend_list);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        // TODO: here change it to friend's list
-        mDataReference = FirebaseDatabase.getInstance().getReference().child("Users");
-
-        // Recycler
-        mRecyclerView = (RecyclerView) findViewById(R.id.friendRecyclerView);
-        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView = (RecyclerView) findViewById(R.id.friendrecyclerView);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
 
-        checkedFriends = new ArrayList<>();
+        // get nFriends here
+        mFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        mDataReference = FirebaseDatabase.getInstance().getReference().child("Users").child(mFirebaseUser.getUid());
 
-    }
-
-    public static class FriendViewHolder extends RecyclerView.ViewHolder {
-
-        View thisView;
-        Switch addSw;
-        EditText email;
-        EditText userN;
-
-        public FriendViewHolder(View itemView, View thisView, Switch addSw, EditText email, EditText userN) {
-            super(itemView);
-            this.thisView = thisView;
-            this.addSw = addSw;
-            this.email = email;
-            this.userN = userN;
-        }
-
-        public void setThisView(View thisView) {
-            this.thisView = thisView;
-        }
-
-        public void setAddSw(Switch addSw) {
-            this.addSw = addSw;
-        }
-
-        public void setEmail(String email) {
-            EditText post_email = (EditText) thisView.findViewById(R.id.row_email);
-            post_email.setText(email);
-        }
-
-        public void setUsern(String usern) {
-            EditText post_userN = (EditText) thisView.findViewById(R.id.row_usern);
-            post_userN.setText(usern);
-        }
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        FirebaseRecyclerAdapter<User, FriendViewHolder> mFirebaseRecAdapter = new FirebaseRecyclerAdapter<User, FriendViewHolder>(
-                User.class,
-                R.layout.single_friend,
-                FriendViewHolder.class,
-                mDataReference
-        ) {
+        ValueEventListener postListener = new ValueEventListener() {
             @Override
-            protected void populateViewHolder(FriendViewHolder viewHolder, final User model, int position) {
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User u = dataSnapshot.getValue(User.class);
+                for (String e : u.getFriendList()) {
+                    User temp = new User();
+                    temp.setEmail(e);
+                    nFriends.add(temp);
+                }
+            }
 
-                viewHolder.setEmail("Email: " + model.getEmail());
-                viewHolder.setUsern("Username: " + model.getUsername());
-
-
-                Switch sButton = (Switch) findViewById(R.id.row_add);
-
-                //Set a CheckedChange Listener for Switch Button
-                sButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
-                    @Override
-                    public void onCheckedChanged(CompoundButton cb, boolean on){
-                        if (on) {
-                            checkedFriends.add(model.getUid());
-                        }
-                        else {
-                            checkedFriends.remove(model.getUid());
-                        }
-                    }
-                });
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                toastMessage("You have not added anyone in your friend's list or Error Occurred");
             }
         };
-        mRecyclerView.setAdapter(mFirebaseRecAdapter);
+
+        nFriendAdapter = new FriendRecycleAdapter(nFriends);
+        mRecyclerView.setAdapter(nFriendAdapter);
     }
 
+    // helper function to toast a message
+    public void toastMessage(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
 }
+
